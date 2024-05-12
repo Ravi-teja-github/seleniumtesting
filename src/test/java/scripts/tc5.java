b332b99318d8b9c17b454c27059046fcf13b4ba0
Ravi <Ravi@DESKTOP-0HRE68P>
2024-05-12 07:56:21 +0530

    new tc5,6 puch

---
+++ src/test/java/scripts/tc5.java
@@ -0,0 +1,11 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class tc5 {
+@Test
+public void demo() {
+	Reporter.log("hello postman api tool",true);
+}
+}
---
+++ src/test/java/scripts/tc6.java
@@ -0,0 +1,11 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class tc6 {
+@Test
+public void demo() {
+	Reporter.log("hello HTTpie api tool",true);
+}
+}
