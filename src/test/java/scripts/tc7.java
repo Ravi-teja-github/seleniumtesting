10c832e3ff42517fc00889f8677f4f9ff3c02686
Ravi <Ravi@DESKTOP-0HRE68P>
2024-05-13 08:45:41 +0530

    new puch

---
+++ src/test/java/scripts/tc7.java
@@ -0,0 +1,11 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class tc7 {
+@Test
+public void demo() {
+	Reporter.log("Hello allu, how ru",true);
+}
+}
